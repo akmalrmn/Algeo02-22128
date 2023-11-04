672b105830a132041bcc2eb8d035fa16622b3ba3
Andhikafdh <andhikafadhillah2211@gmail.com>
2023-11-05 01:50:50 +0700

    feat: add RGBtoHSV function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,41 @@
+def RGBtoHSV(R, G, B) :
+    R = R / 255
+    G = G / 255
+    B = B / 255
+    Cmaks = max(R, G, B)
+    Cmin = min(R, G, B)
+
+    delta = Cmaks - Cmin
+
+    if delta == 0:
+        H = 0
+    elif Cmaks == R:
+        H = (60 * ((G - B) / delta) % 6) 
+    elif Cmaks == G:
+        H = (60 * ((B - R) / delta) + 2)
+    else:
+        H = (60 * ((R - G) / delta) + 4) 
+
+    if Cmaks == 0:
+        S = 0
+    else:
+        S = (delta / Cmaks)
+
+    V = Cmaks 
+
+    return H, S, V
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
